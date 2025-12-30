7f4fc57305ee61dd53adb7e443ef5170d46deeef
sonalitupe16 <sonalitupe16@gmail.com>
2025-12-31 01:13:17 +1100

    Final Code

--- src/test/java/com/api/base/BaseService.java
+++ src/test/java/com/api/base/BaseService.java
@@ -2,6 +2,9 @@ package com.api.base;
 
 import static io.restassured.RestAssured.given;
 
+import com.api.filters.LoggingFilter;
+
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
@@ -19,6 +22,9 @@ public class BaseService {//Wrapper for rest assured !!
 	private final String BASE_URL = "http://64.227.160.186:8080";
 	private RequestSpecification requestSpecification;
 	
+	static {
+		RestAssured.filters(new LoggingFilter());
+	}
 	public BaseService() {
 		requestSpecification = given().baseUri(BASE_URL);
 	}
