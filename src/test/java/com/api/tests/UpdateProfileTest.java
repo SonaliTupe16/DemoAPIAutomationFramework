140c0e9b4183a45c2cecadc34d2e389a09e0c488
sonalitupe16 <sonalitupe16@gmail.com>
2025-12-30 15:50:45 +1100

    Final Upadated

--- src/test/java/com/api/tests/UpdateProfileTest.java
+++ src/test/java/com/api/tests/UpdateProfileTest.java
@@ -1,22 +1,45 @@
 package com.api.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.api.base.AuthService;
 import com.api.base.UserProfileManagementService;
 import com.api.models.request.LoginRequest;
+import com.api.models.request.ProfileRequest;
 import com.api.models.response.LoginResponse;
+import com.api.models.response.ProfileResponse;
 
 import io.restassured.response.Response;
 
-@Test
 public class UpdateProfileTest {
 
-	AuthService authService = new AuthService();
-	Response response = authService.login(new LoginRequest("sonalinikam16", "Bluemoon@1611"));
-	LoginResponse loginResponse = response.as(LoginResponse.class);
-	
-	UserProfileManagementService userProfileManagementService = new UserProfileManagementService();
-	userProfileManagementService.getProfile(loginResponse.getToken());
+	@Test
+	public void updateProfileTest() {
+		AuthService authService = new AuthService();
+		Response response = authService.login(new LoginRequest("sonalinikam16", "Bluemoon@1611"));
+		LoginResponse loginResponse = response.as(LoginResponse.class);
+		System.out.println(response.asPrettyString());
+
+		System.out.println("------------------------------");
+
+		UserProfileManagementService userProfileManagementService = new UserProfileManagementService();
+		response = userProfileManagementService.getProfile(loginResponse.getToken());
+		System.out.println(response.asPrettyString());
+		ProfileResponse profileResponse = response.as(ProfileResponse.class);
+		Assert.assertEquals(profileResponse.getUsername(), "sonalinikam16");
+
+		System.out.println("------------------------------");
+		
+		ProfileRequest profileRequest = new ProfileRequest.Builder()
+				.firstName("Disha")
+				.lastName("Bhat")
+				.email("disha1234@gmail.com")
+				.mobileNumber("7777777774")
+				.build();
+		
+		userProfileManagementService.updateProfile(loginResponse.getToken(),profileRequest);
+		System.out.println(response.asPrettyString());
+	}
 
 }
